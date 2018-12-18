8c55e0b8d8a064216d33aba76eeccd6684a354b0
Josh Johnson <josh@resultsonlyweb.com>
2018-12-18 07:46:16 -0500

    including plugin updaters for github

--- rich-reviews-importer.php
+++ rich-reviews-importer.php
@@ -22,6 +22,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/* ================================================================================================ */
+/*                                  WP Plugin Update Server                                         */
+/* ================================================================================================ */
+
+/**
+* Selectively uncomment the sections below to enable updates with WP Plugin Update Server.
+*
+* WARNING - READ FIRST:
+* Before deploying the plugin or theme, make sure to change the following value
+* - https://your-update-server.com  => The URL of the server where WP Plugin Update Server is installed
+* - $prefix_updater                 => Replace "prefix" in this variable's name with a unique plugin prefix
+*
+* @see https://github.com/froger-me/wp-package-updater
+**/
+
+require_once plugin_dir_path( __FILE__ ) . 'lib/wp-package-updater/class-wp-package-updater.php';
+
+/** Enable plugin updates with license check **/
+// $prefix_updater = new WP_Package_Updater(
+// 	'https://your-update-server.com',
+// 	wp_normalize_path( __FILE__ ),
+// 	wp_normalize_path( plugin_dir_path( __FILE__ ) ),
+// 	true
+// );
+
+/** Enable plugin updates without license check **/
+$rich_reviews_importer_updater = new WP_Package_Updater(
+	'https://github.com/JoshEvanJohnson/rich-reviews-importer',
+	wp_normalize_path( __FILE__ ),
+	wp_normalize_path( plugin_dir_path( __FILE__ ) )
+);
+
+/* ================================================================================================ */
+
 /**
  * Main class
  *
